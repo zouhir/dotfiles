edbf99ee1fc49a7e2951fac8478c924b78eaab64
Zouhir <zouhir@zouhir.org>
2023-01-15 18:47:21 -0800

    cleanup no longer needed tools, create alacritty configs, embed Google aliases

--- private_dot_config/private_fish/abbreviations.fish
+++
@@ -1,4 +0,0 @@
-# faster, better find
-if command -qs fd
-	abbr -a find 'fd'
-end
--- private_dot_config/private_fish/functions/cat.fish
+++
@@ -1,8 +0,0 @@
-function cat --description 'Replace cat with bat'
-  if command -qa bat
-	  abbr -a cat 'bat'
-  end
-  if command -qa batcat
-    abbr -a cat 'batcat'
-  end
-end
