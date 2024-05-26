183b090e4259326fb11b81acc713eb939aea8802
Zouhir Chahoud <zouhir@google.com>
2024-05-25 23:34:48 -0700

    switch to fish

--- default/dotfiles/.config/fish/functions/fish_prompt.fish
+++ default/dotfiles/.config/fish/functions/fish_prompt.fish
@@ -1,17 +1,24 @@
-function fish_prompt
-    set_color cyan
-    if test "$SSH_CLIENT" = ""    # Check if connected via SSH
-        echo -n (whoami) "@" (hostname | cut -d . -f 1) " "
-        set_color yellow
-        echo -n " " (prompt_pwd)
-    else 
-        echo -n "@" (hostname) " " 
+# name: Informative
+# http://michal.karzynski.pl/blog/2009/11/19/my-informative-shell-prompt/
+# https://github.com/fish-shell/fish-shell/blob/6aade380c1abc28414e98c60fe06c112447d7fe9/share/tools/web_config/sample_prompts/informative.fish
+
+function fish_prompt --description 'Informative prompt'
+    #Save the return status of the previous command
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    if functions -q fish_is_root_user; and fish_is_root_user
+        printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
+                                                         and set_color $fish_color_cwd_root
+                                                         or set_color $fish_color_cwd) \
+            (prompt_pwd) (set_color normal)
+    else
+        set -l status_color (set_color $fish_color_status)
+        set -l statusb_color (set_color --bold $fish_color_status)
+        set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+        printf '[%s] %s%s@%s %s%s %s%s%s \n❯ ' (date "+%H:%M:%S") (set_color brblue) \
+            $USER (hostname -s) (set_color $fish_color_cwd) $PWD $pipestatus_string \
+            (set_color normal)
     end
-
-    # New Line with Unicode Character
-    echo " ❯"  # Unicode character U+276F
-
-    set_color green
-    echo " \$ "
-
 end
